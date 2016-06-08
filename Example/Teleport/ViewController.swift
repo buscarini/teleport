87da1a8e257252af61996fc0c0d697458975b609
José Manuel <buscarini@gmail.com>
2016-06-08 10:53:19 +0200

    Added navigation controller to example

--- Example/Teleport/ViewController.swift
+++ Example/Teleport/ViewController.swift
@@ -13,11 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+		
+		self.title = "VC1"
     }
 
 	@IBAction func goto2(sender: AnyObject?) {
 		let delegate = UIApplication.sharedApplication().delegate as! AppDelegate
-		delegate.navigate(.ViewController(ViewController2.self, child: nil))
+		delegate.navigate(
+			.NavigationController(
+				[
+					.ViewController(ViewController2.self, child: nil),
+					.ViewController(ViewController2.self, child: nil),
+				]
+			)
+		)
 	}
 }
 
